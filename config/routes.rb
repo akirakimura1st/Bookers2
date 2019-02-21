ff0aa71bcad1055da5e59a4f1b18d717b7bf6fd6
akirakimura1st <girlsbandparty1th@gmail.com>
2019-02-22 04:19:54 +0900

    Hasn't finished yet.

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/home'
+  get 'users/home'
+  get 'users/index'
+  get 'users/edit'
+  get 'users/show'
+  get 'books/show'
+  get 'books/index'
+  get 'books/new'
+  get 'books/edit'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
